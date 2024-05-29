a210fb1cf43ddd556bd532297b5eed6a154146ef
weakinyi <akinyiwendy46@gmail.com>
2024-05-29 14:12:42 +0300

    feat: calculating the median of the data.txt file provided

---
+++ mathskills/median.go
@@ -0,0 +1,11 @@
+package mathskills
+
+func Median(num []float64) float64 {
+	n := len(num)
+	//median for if the length of the slive is an even number
+	if n%2 == 0 {
+		return (num[n/2-1] + num[n/2]) / 2
+	}
+	//median for if the length of the slice is an odd number
+	return num[n/2]
+}
