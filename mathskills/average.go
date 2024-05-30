11aa8fe076b1af2e390b585d6dd6ebf4975bede1
weakinyi <akinyiwendy46@gmail.com>
2024-05-30 15:16:46 +0300

    feat: modified some lines in the code

--- mathskills/average.go
+++ mathskills/average.go
@@ -2,8 +2,9 @@ package mathskills
 
 func Average(num []float64) float64 {
 	total := 0.0
+	n := len(num)
 	for _, nb := range num {
 		total += nb
 	}
-	return total / float64(len(num))
+	return total / float64(n)
 }
