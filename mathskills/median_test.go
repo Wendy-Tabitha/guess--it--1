9bc608fbc1e9edd66032e773f6baf94c67dea90a
weakinyi <akinyiwendy46@gmail.com>
2024-05-30 16:35:24 +0300

    test: this is a test file for median function

---
+++ mathskills/median_test.go
@@ -0,0 +1,21 @@
+package mathskills
+
+import (
+	"math"
+	"reflect"
+	"sort"
+	"testing"
+)
+
+func TestMedian(t *testing.T) {
+	input := []float64{10, 30, 20, 50, 40}
+	expected := 30.0
+	
+	sort.Float64s(input)
+	result := Median(input)
+	roundOutput := math.Round(result)
+
+	if !reflect.DeepEqual(roundOutput, expected) {
+		t.Errorf("Expected %v, but got %v", expected, roundOutput)
+	}
+}
